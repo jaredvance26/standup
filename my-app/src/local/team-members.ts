02fb14d3c72f537ef00817f833f7fdd1c3761de1
Jared Vance <74557401+jaredvance26@users.noreply.github.com>
2025-03-20 16:17:19 -0600

    header done

---
+++ my-app/src/local/team-members.ts
@@ -0,0 +1,75 @@
+import { TeamMember } from "../types";
+
+import { ad, bb, bz, cr, cs, dc, ds, jp, jv, rp } from "./images";
+export const teamMembers: TeamMember[] = [
+  {
+    firstName: "Jared",
+    lastName: "Vance",
+    photoUrl: jv,
+    jiraId: "6279c3f123d61e006fc419ad",
+    position: "Software Engineer",
+  },
+  {
+    firstName: "Randi",
+    lastName: "Prince",
+    photoUrl: rp,
+    jiraId: "712020:7376fb19-3b5e-4b6a-a917-7d592d5e3c63",
+    position: "Software Engineer 2",
+  },
+  {
+    firstName: "Jake",
+    lastName: "Pennington",
+    photoUrl: jp,
+    jiraId: "5ec1e475c2e9a00c28b31ac5",
+    position: "Senior Software Engineer",
+  },
+  {
+    firstName: "Angela",
+    lastName: "Dye",
+    photoUrl: ad,
+    jiraId: "557058:1ba1111b-5864-4157-89dc-21f37a6f4fd8",
+    position: "QA Engineer",
+  },
+  {
+    firstName: "Brody",
+    lastName: "Binks",
+    photoUrl: bb,
+    jiraId: "712020:34911af8-48bb-4e47-90ed-312064bb9522",
+    position: "Senior Software Engineer",
+  },
+  {
+    firstName: "Brian",
+    lastName: "Zwahr",
+    photoUrl: bz,
+    jiraId: "61c0fd8df63ac80070aa713a",
+    position: "Senior Software Engineer",
+  },
+  {
+    firstName: "Chris",
+    lastName: "Russon",
+    photoUrl: cr,
+    jiraId: "712020:0726456e-714f-45f4-ac25-9ba95929ab86",
+    position: "Product Manager 2",
+  },
+  {
+    firstName: "Clayton",
+    lastName: "Sharp",
+    photoUrl: cs,
+    jiraId: "712020:a178e327-d042-4614-bf95-9323e1f3c54b",
+    position: "Software Developer",
+  },
+  {
+    firstName: "Dave",
+    lastName: "Summers",
+    photoUrl: ds,
+    jiraId: "5b928bb1d5f27c2c1933d447",
+    position: "Engineering Manager",
+  },
+  {
+    firstName: "Devin",
+    lastName: "Cameron",
+    photoUrl: dc,
+    jiraId: "60458bd7e19f910069247cce",
+    position: "Senior Product Designer",
+  },
+];
---
+++ my-app/src/standup-wizard/components/standup-wizard-header.tsx
@@ -0,0 +1,24 @@
+import { Box, Typography } from "@mui/material";
+import React, { ReactElement } from "react";
+
+import roundTable from "../images/round-table.png";
+import { TEAM } from "../../local";
+
+export const StandupWizardHeader = (): ReactElement => {
+  return (
+    <Box
+      display="flex"
+      gap={2}
+      justifyContent="center"
+      alignItems="center"
+      marginBottom={6}
+    >
+      <img src={roundTable} alt="round table" />
+      <Typography
+        textAlign="center"
+        fontSize={32}
+        fontWeight={500}
+      >{`${TEAM.name} Standup`}</Typography>
+    </Box>
+  );
+};
