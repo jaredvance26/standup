de043bf8c13112702d463dec4fa654597dcc3aa2
Jared Vance <74557401+jaredvance26@users.noreply.github.com>
2025-04-18 15:59:21 -0600

    added blank state to jira section

--- my-app/src/standup-wizard/steps/standup/components/jira-section.tsx
+++ my-app/src/standup-wizard/steps/standup/components/jira-section.tsx
@@ -4,6 +4,7 @@ import { format, parseISO } from "date-fns";
 
 import { JiraIssue, Sprint } from "../../../types/jira";
 import { JiraIssues } from "./jira-issues";
+import { Coffee } from "@mui/icons-material";
 
 interface JiraSectionProps {
   sprint: Sprint;
@@ -15,10 +16,11 @@ export const JiraSection = (props: JiraSectionProps): ReactElement => {
   const { palette } = useTheme();
 
   const endDate = parseISO(sprint.endDate);
+  console.log({ issues });
 
   return (
     <Box flex={1} alignSelf="start">
-      <Box display='flex' flexDirection="column" gap={2}>
+      <Box display="flex" flexDirection="column" gap={2}>
         <Box
           sx={{
             backgroundColor: palette.grey[300],
@@ -26,13 +28,13 @@ export const JiraSection = (props: JiraSectionProps): ReactElement => {
             padding: 2,
           }}
         >
-          <Box display="flex" alignItems="end" gap={1} justifyContent="center">
+          <Box display="flex" alignItems="end" gap={1}>
             <Typography
               textAlign="center"
               fontSize={24}
               fontWeight={600}
               marginBottom={0.5}
-			  color={palette.primary.dark}
+              color={palette.primary.dark}
             >
               {sprint.name}
             </Typography>
@@ -48,17 +50,39 @@ export const JiraSection = (props: JiraSectionProps): ReactElement => {
               )}`}
             </Typography>
           </Box>
-          <Box display="flex" justifyContent="center">
+          <Box display="flex">
             <Typography whiteSpace="pre-line" fontSize={14} fontWeight={500}>
               {sprint.goals}
             </Typography>
           </Box>
         </Box>
-        <Box
-          height="400px"
-          overflow="auto"
-        >
-          <JiraIssues issues={issues} />
+        <Box height="400px" overflow="auto">
+          {issues.length ? (
+            <JiraIssues issues={issues} />
+          ) : (
+            <Box
+              display="flex"
+              flexDirection="column"
+              alignItems="center"
+              justifyContent="center"
+              height="100%"
+              sx={{
+                "& > *": {
+                  opacity: 0.5,
+                },
+                backgroundColor: palette.grey[200],
+                borderRadius: 3,
+              }}
+            >
+              <Coffee sx={{ fontSize: 100 }} />
+              <Typography marginTop={2} fontSize={24} fontWeight={500}>
+                Taking a breather from tickets
+              </Typography>
+              <Typography fontSize={16} color="text.secondary">
+                Sometimes the best work happens outside of Jira
+              </Typography>
+            </Box>
+          )}
         </Box>
       </Box>
     </Box>
